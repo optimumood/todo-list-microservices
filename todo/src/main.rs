4f545d67820d6ec7a82bbc206daeebc2ca58a756
optimumood <optimumood@gmail.com>
2023-04-12 22:38:09 +0200

    Skeleton for todo app HTTP handlers

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,6 +1,48 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, patch},
+    Router,
+};
+use std::net::SocketAddr;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tracing::Level;
 
-fn main() {
-    loop {
-        std::thread::sleep(std::time::Duration::from_secs(10));
-    }
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .init();
+
+    let app = Router::new()
+        .route("/todos", get(todos_list).post(todos_create))
+        .route("/todos/:id", patch(todos_update).delete(todos_delete))
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        );
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn todos_list() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
+}
+
+async fn todos_create() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
+}
+
+async fn todos_update() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
+}
+
+async fn todos_delete() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
 }
